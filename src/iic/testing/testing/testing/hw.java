1caec34b4c6531ecb97d80ef5f952a2e5d0706f1
kusala9 <kusala9@googlemail.com>
2025-04-24 11:10:17 +0100

    initial pop for v1.0.2

--- src/iic/testing/testing/testing/hw.java
+++ src/iic/testing/testing/testing/hw.java
@@ -2,7 +2,7 @@ package iic.testing.testing.testing;
 
 public class hw
 {
-    public static final String VER="1.0.1";
+    public static final String VER="1.0.2";
     public static String hw()
     {
         return "hello world";
